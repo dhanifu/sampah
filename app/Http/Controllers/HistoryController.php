894b9d6d9a67893a1ff00fba80124ebfe0801664
Muhammad Ramdhani <mramdhani010@gmail.com>
2021-02-15 09:04:40 +0700

    filter date method, etc.

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 use App\Models\Trash;
 use App\Models\TrashDetail;
@@ -11,7 +12,20 @@ class HistoryController extends Controller
 {
     public function index()
     {
-        return view('operator.transaction.history.index');
+        $start = Carbon::now()->startOfMonth()->format('Y-m-d H:i:s');
+        $end = Carbon::now()->endOfMonth()->format('Y-m-d h:i:s');
+
+        if ( request()->date != '' ) {
+            $date = explode(' - ', request()->date);
+            $start = Carbon::parse($date[0])->format('Y-m-d'). ' 00:00:01';
+            $end = Carbon::parse($date[1])->format('Y-m-d'). ' 23:59:59';
+        }
+
+        $histories = Trash::select(['id','user_id','member_id','date','weight','total'])
+                    ->whereBetween('created_at', [$start, $end])
+                    ->with(['user:id,name','member.village:id,name'])->paginate(10);
+        
+        return view('operator.transaction.history.index', compact('histories'));
     }
 
     public function detail(Request $request)
@@ -30,4 +44,35 @@ class HistoryController extends Controller
         return view('operator.transaction.history.detail', compact('data','transaction'));
     }
 
+    public function destroy(Trash $trash)
+    {
+        try {
+            TrashDetail::select('trash_id')->where('trash_id',$trash->id)->delete();
+            $trash->delete();
+
+            return back()->with('success', 'berhasil dihapus ke tempat sampah');
+        } catch (\Illuminate\Database\QueryException $e) {
+            return back()->with('error', 'gagal menghapus');
+        }
+    }
+
+
+    // TRASH
+    public function trash()
+    {
+        $histories = Trash::onlyTrashed()->select(['id','user_id','member_id','date','weight','total'])
+                        ->latest()->with(['user:id,name','member.village:id,name'])
+                        ->paginate(10);
+        return view('operator.transaction.trash', compact('histories'));
+    }
+
+    public function restoreData($id)
+    {
+        // 
+    }
+
+    public function deleteData($id)
+    {
+        // 
+    }
 }
