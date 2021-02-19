d07a24710cf2482d2c45d1ca96ffc76e7060b157
Muhammad Ramdhani <mramdhani010@gmail.com>
2021-02-19 22:15:08 +0700

    rapihin kode

--- public/js/history-trash.js
+++ public/js/history-trash.js
@@ -1,182 +1,188 @@
 jQuery(function ($) {
-
     const table = $("table").DataTable({
         serverSide: true,
         processing: true,
         ajax: {
             url: ajaxUrl,
-            type: 'get',
+            type: "get",
         },
         columns: [
-            { data: 'DT_RowIndex' },
-            { data: 'operator' },
-            { data: 'member' },
-            { data: 'date' },
-            { data: 'weight' },
-            { data: 'total' },
+            { data: "DT_RowIndex" },
+            { data: "operator" },
+            { data: "member" },
+            { data: "date" },
+            { data: "weight" },
+            { data: "total" },
             {
-                data: 'action',
+                data: "action",
                 orderable: false,
-                searchable: false
-            }
-        ]
-    })
+                searchable: false,
+            },
+        ],
+    });
 
-    const reload = () => table.ajax.reload()
+    const reload = () => table.ajax.reload();
 
-    const success = msg => {
+    const success = (msg) => {
         Swal.fire({
-            icon: 'success',
+            icon: "success",
             title: msg,
             showConfirmButton: false,
             timer: 1500,
-            timerProgressBar: true
-        })
-        reload()
-    }
+            timerProgressBar: true,
+        });
+        reload();
+    };
 
-    const error = msg => {
+    const error = (msg) => {
         Swal.fire({
-            icon: 'error',
+            icon: "error",
             title: msg,
             showConfirmButton: false,
             timer: 1500,
-            timerProgressBar: true
-        })
-        reload()
-    }
+            timerProgressBar: true,
+        });
+        reload();
+    };
 
-    const restore = id => {
-        const url = restoreUrl.replace(':id', id)
+    const restore = (id) => {
+        const url = restoreUrl.replace(":id", id);
         Swal.fire({
             title: "Are you sure?",
             text: "Aksi ini akan merestore data",
             icon: "warning",
             showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, restore it!',
-            allowOutsideClick: false
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, restore it!",
+            allowOutsideClick: false,
         }).then((result) => {
             if (result.value) {
                 $.ajax({
                     url: url,
-                    type: 'get',
-                    success: res => {
-                        success(res.success)
-                        $("#history-count-trash").html(res.count)
-                    }
-                })
+                    type: "get",
+                    success: (res) => {
+                        success(res.success);
+                        setTimeout(() => {
+                            $("#history-count-trash").html(res.count);
+                        }, 2000);
+                    },
+                });
             }
         });
-    }
+    };
 
-    const remove = id => {
-        const url = deleteUrl.replace(':id', id)
+    const remove = (id) => {
+        const url = deleteUrl.replace(":id", id);
         Swal.fire({
             title: "Apakah Anda yakin?",
             text: "Aksi ini akan menghapus data secara permanen",
             icon: "warning",
             showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!',
-            allowOutsideClick: false
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!",
+            allowOutsideClick: false,
         }).then((result) => {
             if (result.value) {
                 $.ajax({
                     url: url,
-                    type: 'get',
-                    success: res => {
-                        success(res.success)
-                        $("#history-count-trash").html(res.count)
-                    }
-                })
+                    type: "get",
+                    success: (res) => {
+                        success(res.success);
+                        setTimeout(() => {
+                            $("#history-count-trash").html(res.count);
+                        }, 2000);
+                    },
+                });
             }
         });
-    }
+    };
 
     const restoreAll = () => {
-        const url = restoreAllUrl
+        const url = restoreAllUrl;
         Swal.fire({
             title: "Are you sure?",
             text: "Aksi ini akan merestore semua data",
             icon: "warning",
             showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, restore it!',
-            allowOutsideClick: false
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, restore it!",
+            allowOutsideClick: false,
         }).then((result) => {
             if (result.value) {
                 $.ajax({
                     url: url,
-                    type: 'get',
-                    success: res => {
-                        success(res.success)
-                        $("#history-count-trash").html(res.count)
+                    type: "get",
+                    success: (res) => {
+                        success(res.success);
+                        setTimeout(() => {
+                            $("#history-count-trash").html(res.count);
+                        }, 2000);
                     },
-                    error: err => {
-                        console.log(err)
-                    }
-                })
+                    error: (err) => {
+                        console.log(err);
+                    },
+                });
             }
         });
-    }
+    };
 
     const removeAll = () => {
-        const url = deleteAllUrl
+        const url = deleteAllUrl;
         Swal.fire({
             title: "Apakah Anda yakin?",
             text: "Aksi ini akan menghapus semua data secara permanen",
             icon: "warning",
             showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!',
-            allowOutsideClick: false
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!",
+            allowOutsideClick: false,
         }).then((result) => {
             if (result.value) {
                 $.ajax({
                     url: url,
-                    type: 'get',
-                    success: res => {
-                        success(res.success)
-                        $("#history-count-trash").html(res.count)
-                    }
-                })
+                    type: "get",
+                    success: (res) => {
+                        success(res.success);
+                        setTimeout(() => {
+                            $("#history-count-trash").html(res.count);
+                        }, 2000);
+                    },
+                });
             }
         });
-    }
+    };
 
-    $('tbody').on('click', 'button', function () {
-        const action = $(this).data('action')
-        const data = table.row($(this).parents('tr')).data()
-
-        switch (action) {
-            case 'restore':
-                restore(data.id)
-                break
-            case 'remove':
-                remove(data.id)
-                break
-        }
-    })
-
-
-    $(".card-tools").on('click', 'button', function () {
-        const action = $(this).data('action')
+    $("tbody").on("click", "button", function () {
+        const action = $(this).data("action");
+        const data = table.row($(this).parents("tr")).data();
 
         switch (action) {
-            case 'refresh':
-                reload()
-                break
-            case 'restore-all':
-                restoreAll()
-                break
-            case 'remove-all':
-                removeAll()
-                break
+            case "restore":
+                restore(data.id);
+                break;
+            case "remove":
+                remove(data.id);
+                break;
         }
-    })
-})
+    });
+
+    $(".card-tools").on("click", "button", function () {
+        const action = $(this).data("action");
+
+        switch (action) {
+            case "refresh":
+                reload();
+                break;
+            case "restore-all":
+                restoreAll();
+                break;
+            case "remove-all":
+                removeAll();
+                break;
+        }
+    });
+});
--- public/js/history.js
+++ public/js/history.js
@@ -1,113 +1,124 @@
 jQuery(function ($) {
-
-    const table = (from_date = '', to_date = '') => {
+    const table = (from_date = "", to_date = "") => {
         $("table").DataTable({
             serverSide: true,
             processing: true,
             ajax: {
                 url: ajaxUrl,
-                type: 'get',
+                type: "get",
                 data: {
                     from_date: from_date,
-                    to_date: to_date
+                    to_date: to_date,
                 },
-                error: err => {
-                    console.log(err)
-                }
+                error: (err) => {
+                    console.log(err);
+                },
             },
             columns: [
-                { data: 'DT_RowIndex' },
-                { data: 'operator' },
-                { data: 'member' },
-                { data: 'date' },
-                { data: 'weight' },
-                { data: 'total' },
+                { data: "DT_RowIndex" },
+                { data: "operator" },
+                { data: "member" },
+                { data: "date" },
+                { data: "weight" },
+                { data: "total" },
                 {
-                    data: 'action',
+                    data: "action",
                     orderable: false,
-                    searchable: false
-                }
-            ]
-        })
-    }
+                    searchable: false,
+                },
+            ],
+        });
+    };
 
-    table()
+    table();
 
     const refresh = () => {
-        $('#from_date').val('')
-        $('#to_date').val('')
-        $('table').DataTable().destroy()
-        table()
-    }
+        $("#from_date").val("");
+        $("#to_date").val("");
+        $("table").DataTable().destroy();
+        table();
+    };
 
-    $("#refresh").on('click', function () {
-        refresh()
-    })
+    $("#refresh").on("click", function () {
+        refresh();
+    });
 
-    const success = msg => {
+    const success = (msg) => {
         Swal.fire({
-            icon: 'success',
+            icon: "success",
             title: msg,
             showConfirmButton: false,
             timer: 1500,
-            timerProgressBar: true
-        })
+            timerProgressBar: true,
+        });
 
-        refresh()
-    }
-    const error = msg => {
+        refresh();
+    };
+    const error = (msg) => {
         Swal.fire({
-            icon: 'error',
+            icon: "error",
             title: msg,
             showConfirmButton: false,
             timer: 1500,
-            timerProgressBar: true
-        })
-    }
+            timerProgressBar: true,
+        });
+    };
 
-    const remove = id => {
+    const remove = (id) => {
         Swal.fire({
             title: "Are you sure?",
             icon: "warning",
             showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!',
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!",
             allowOutsideClick: false,
         }).then((result) => {
             if (result.value) {
-                const url = deleteUrl.replace(':id', id)
+                const url = deleteUrl.replace(":id", id);
 
                 $.ajax({
                     url: url,
-                    type: 'post',
+                    type: "post",
                     data: {
                         _token: csrf,
-                        _method: 'DELETE'
+                        _method: "DELETE",
                     },
-                    success: res => {
-                        $("#history-count-trash").html(res.count)
-                        success(res.success)
+                    success: (res) => {
+                        success(res.success);
+                        setTimeout(function () {
+                            $("#history-count-trash").html(res.count);
+                        }, 2000);
                     },
-                })
+                });
             }
         });
-    }
+    };
 
+    $("#filter").on("click", function () {
+        let from_date = $("#from_date").val();
+        let to_date = $("#to_date").val();
 
-    $('#filter').on('click', function () {
-        let from_date = $('#from_date').val()
-        let to_date = $('#to_date').val()
-        if (from_date != '' && to_date != '') {
-            $('table').DataTable().destroy()
-            table(from_date, to_date)
+        let url = pdfUrl.replace(":date", `${from_date}+${to_date}`);
+
+        if (from_date != "" && to_date != "") {
+            $("table").DataTable().destroy();
+            table(from_date, to_date);
+
+            setTimeout(() => {
+                $("#exportpdf").attr("disabled", false);
+                $("#exportexcel").attr("disabled", false);
+                $("#exportpdf").on("click", function () {
+                    document.location.href = url;
+                });
+            }, 700);
         } else {
-            error('Pilih daterange terlebih dahulu')
+            error("Pilih daterange terlebih dahulu");
         }
-    })
+    });
 
-    $('tbody').on('click', '#remove', function () {
-        const id = $(this).attr('data-id')
-        remove(id)
-    })
-})
+    $("tbody").on("click", "#remove", function () {
+        const id = $(this).attr("data-id");
+        remove(id);
+    });
+});
