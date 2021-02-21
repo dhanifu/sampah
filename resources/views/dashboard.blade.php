401930fd06ad91a0f54b30d4ce88bd4a3015151d
Muhammad Ramdhani <mramdhani010@gmail.com>
2021-02-21 22:09:44 +0700

    mindahin fungsi greeting ke helpers

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,7 +19,7 @@
     <div class="col-lg-12">
         <div class="card">
             <div class="card-header">
-                <h5 class="m-0">Halo, {{ $greeting }}</h5>
+                <h5 class="m-0">{{ greeting(Auth::user()->name) }}</h5>
             </div>
             <div class="card-body">
                 <h6 class="card-title">Special title treatment</h6>
